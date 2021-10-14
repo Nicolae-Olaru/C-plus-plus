909a994914ddc25498a93bf3d53f325b99da46d9
Nicolae Olaru <61246526+Nicolae-Olaru@users.noreply.github.com>
2021-10-14 11:38:40 +0200

    Delete UNINFORMED directory

--- UNINFORMED/MATRIX/Load_Matrix_0.c
+++
@@ -1,8 +0,0 @@
-
-void Load_Matrix_0(int matrix1[][10],int dim)
-{
-    int g,m;
-    for(g=0;g<dim;g++)
-        for(m=0;m<dim;m++)
-            matrix1[g][m] = 0;
-}
--- UNINFORMED/MATRIX/Play_Load_Matrix_2_DImensions.c
+++
@@ -1,8 +0,0 @@
-
-void Load_matrix_play(int matrix1[][10],int dim)
-{
-    int g,m;
-    for(g=0;g<dim;g++)
-        for(m=(dim-1);m>-1;m--)
-            matrix1[m][g] = 99;
-}
